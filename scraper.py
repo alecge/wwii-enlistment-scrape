996ebe97f964425d4c29c0334249faab9fe61bc6
Alec Ge <me@alec.ge>
2018-06-03 19:06:31 -0400

    Refined the way the url was being generated

--- scraper.py
+++ scraper.py
@@ -3,32 +3,56 @@ import csv
 
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
+from typing import List
+from typing import Tuple
 
 # Set the driver to the prebuilt docker container running on the same machine
-print("browser bois?")
 browser = webdriver.Remote(command_executor='http://localhost:4444/wd/hub',
                            desired_capabilities=DesiredCapabilities.CHROME)
-print("huh")
 
 
-def generate_field_params(initial_id: int) -> str:
-    params = list(str)
+def generate_field_params(initial_id: int) -> Tuple[str, int]:
+    params: List[str] = list()
 
-    for i in range (initial_id, initial_id + 7):
-        if i not in constants.CONSTANT_FIELDS:
-            list.append('sc=')
-            list.append(i)
-            list.append('&')
-    
-    return ''.join(params)
+    # Append the field IDs that will always be needed
+    for field in constants.CONSTANT_FIELDS:
+        params.append('sc=')
+        params.append(str(field))
+        params.append('&')
+
+    cur_id = initial_id
+    num_params = 0
+
+    # Do while loop
+    # If
+    while True:
+        params.append('sc=')
+
+        # If the current field ID (cur_id) is not already added and it is smaller than the largest possible ID,
+        # Add the current field ID to the params list
+        if cur_id not in params and cur_id <= constants.ENDING_ID:
+            params.append(cur_id)
+            num_params += 1
+
+        cur_id += 1
+
+        # If we are at the maximum number of parameters and
+        if num_params >= 7:
+            break
+        else:
+            params.append('&')
+
+    return ''.join(params), int(params[-1])
 
 
-def get_state_ids() -> None:
+def populate_state_ids() -> None:
     with open(constants.STATE_ID_FILE_NAME) as state_id_file:
         data = csv.reader(state_id_file)
         for row in data:
-            constants.state_ids.append(row[0])
+            constants.STATE_IDS.append(row[0])
+
+    # Pop the first item off because the csv file's first row is "Code"
+    constants.STATE_IDS = constants.STATE_IDS[1:]
 
 
 def get_data_from_fields(url: str) -> None:
