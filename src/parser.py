270805ee7ed8a19afd3003f8bd39d2c1ceac87a1
Alec Ge <me@alec.ge>
2018-09-05 21:53:42 -0400

    Further layout of parser and data structures

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,37 @@
+from pathlib import Path
+from typing import List
+
 from bs4 import BeautifulSoup
-import sqlite3
+from bs4.element import Tag
+
+from record import Record
+
 
 class Parser:
-    pass
+    def __init__(self, path: str):
+        self.__records: List[Record] = list()
+        self.__hTreeml: BeautifulSoup = None
+
+        self.doc_fp = Path(path).open()
+        self.__hTreeml = BeautifulSoup(self.doc_fp)
+
+    def __process_records(self) -> List[Record]:
+        table = self.__hTreeml['queryResults']
+
+        thead: Tag = table.thead
+        tbody: Tag = table.tbody
+
+        for tr in tbody.children:
+            
+
+
+
+
+
+        return None
+
+    def get_records(self) -> List[Record]:
+        if not self.__records:
+            self.__process_records()
+
+        return self.__records
--- src/record.py
+++ src/record.py
@@ -1,3 +1,9 @@
+from typing import Dict
+
+
 class Record:
     def __init__(self):
+        self.__data: Dict[str, str] = Dict[str, str]()
+
+
         pass
