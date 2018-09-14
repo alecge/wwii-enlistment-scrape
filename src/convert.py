7c851dfd7d80e2003287ab266305def92feeb3ae
Alec Ge <me@alec.ge>
2018-09-13 20:27:42 -0400

    Converter fixes

--- src/convert.py
+++ src/convert.py
@@ -2,10 +2,11 @@ from records_to_csv import RecordsToCsv
 from parser import Parser
 from pathlib import Path
 import logging
+from typing import Set
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.INFO)
 
 
 def convert_to_csv():
@@ -14,19 +15,22 @@ def convert_to_csv():
     folder_path: Path = Path('/home/alec/scraped-data').resolve(strict=True)
 
     first_run = True
+    files_read: Set[str] = set()
 
     for subfolder in folder_path.iterdir():
-        log.debug('Working through ' + str(subfolder))
+        log.info('Working through ' + str(subfolder))
         if subfolder.is_dir():
             for file in subfolder.iterdir():
-                records = Parser(str(file)).parse()
 
-                if first_run:
-                    first_run = False
-                    converter.init_csv(records[0].get_field_names())
-                    print(records[0].get_field_names())
+                if file.parts[-1] not in files_read:
+                    records = Parser(str(file)).parse()
+                    files_read.add(file.parts[-1])
+                    if first_run:
+                        first_run = False
+                        converter.init_csv(records[0].get_field_names())
 
-                converter.add_to_csv(*records)
+                    log.debug('Parsing file ' + file.parts[-1])
+                    converter.add_to_csv(*records)
 
     converter.done()
 
